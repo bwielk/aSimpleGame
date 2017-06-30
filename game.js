a70cf1fc19578ff0deb9af3ed6f9901058e9eaee
bwielk <bwielk@gmail.com>
2017-06-30 16:24:27 +0200

    further work

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+var Score = require('./score');
+
 var Game = function(name) {
     this.name = name
     this.players = []
@@ -13,8 +15,16 @@ Game.prototype = {
     },
 
     play: function() {
-
+    	var scores = [];
+    	for(var player of this.players){
+    		var value = new Score();
+    		scores.push(value.calculate(player));
+    	}
+   		var res = Math.max.apply(Math,array.map(function(o){return o.value;}))
+   		console.log(scores);
+   		return "" + res.name + " wins!";
     }
+
 };
 
 module.exports = Game;
